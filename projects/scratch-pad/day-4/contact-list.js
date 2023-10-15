503cdba27fbba82a3b7d0ba836f5d86e1fda76d5
Jay504Media Brown <Jermaineb289@gmail.com>
2023-10-15 21:33:19 +0000

    done with day 4 homework

--- projects/scratch-pad/day-4/contact-list.js
+++ projects/scratch-pad/day-4/contact-list.js
@@ -35,10 +35,13 @@
 
 // YOUR CODE GOES BELOW HERE //
 function makeContact(id, nameFirst, nameLast) {
-    
-
+    return {
+        id: id,
+        nameFirst: nameFirst,
+        nameLast: nameLast
+    };
 } 
-
+ 
 function makeContactList() {
     /*
      * You need something here to hold contacts. See length api for a hint:
@@ -46,20 +49,43 @@ function makeContactList() {
     var contacts = [];
     
     return {
-        // we implemented the length api for you //
+ // we implemented the length api for you //
         length: function() {
             return contacts.length;
         },
-        addContact: function(contact)
-    }
-}
-
-
-
-
+        addContact: function(contact) {
+         contacts.push(contact);
+        },
+        findContact: function(fullName) {
+         for (var i = 0; i < contacts.length; i++) {
+          var contact = contacts[i];
+          if (contact.nameFirst.toLowerCase() + ' ' + contact.nameLast.toLowerCase() === fullName.toLowerCase()) {
+            return contact;
+          }
+       }
+           return undefined;
+    },
+        removeContact: function(contact)  {
+          var index = contacts.indexOf(contact);
+         if (index !== -1) {
+            contacts.splice(index, 1);
+         }
+        },
+        printAllContactNames: function() {
+        
+            var names = ''; 
+               for (var i = 0; i < contacts.length; i++) {
+                 var contact = contacts[i]; names += contact.nameFirst + ' ' + contact.nameLast;
+                  if (i !== contacts.length -1) {
+                         names += '\n';
+                  }
+                }   
+                  return names;
 // YOUR CODE GOES ABOVE HERE //
-
-
+       
+      }
+   }; 
+}
 
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
